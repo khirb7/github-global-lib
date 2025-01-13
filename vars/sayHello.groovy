664e16cb700eaeafeed42fe7e13efa96d5ad8e1e
khirb7 <yahiaoui.kh@gmail.com>
2025-01-13 14:51:33 +0100

    Update sayHello.groovy
    update

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(Map config = [:]){
-   echo "Hello, ${config.name}. Today is ${config.dayOfWeeek}"
+   echo "Hello, ${config.name}. Today is ${config.dayOfWeek}"
 }
